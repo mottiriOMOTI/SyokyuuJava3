46fc65e24a93041a1bd3a3efc6a560696c6535fd
MOTTTIRIomoti <zeal23310168@fsgcl.jp>
2024-05-13 11:55:34 +0900

    初級JAVA2.5の追加

---
+++ 初級JAVA2.5/src/Start.java
@@ -0,0 +1,16 @@
+
+public class Start {
+	public static void main(String[] args) {
+			int x = 2,y = 3;
+			x += y;
+			System.out.println(x);
+	MyFrame frame1 = new MyFrame();
+	frame1.setLocation(200,100);
+	frame1.setSize(150,150);
+	MyFrame frame2 = new RectFrame();
+	frame2.setLocation(350,200);
+	MyFrame frame3 = new RectFrame();
+	frame2.setLocation(50,300);
+	}
+
+}
