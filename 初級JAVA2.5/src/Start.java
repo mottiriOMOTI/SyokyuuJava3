a3e9f7408e5ad7a2a5f78619d6240de9e909b811
MOTTTIRIomoti <zeal23310168@fsgcl.jp>
2024-05-14 10:18:32 +0900

    no message

--- 初級JAVA2.5/src/Start.java
+++ 初級JAVA2.5/src/Start.java
@@ -1,9 +1,9 @@
 
 public class Start {
 	public static void main(String[] args) {
-			int x = 2,y = 3;
-			x += y;
-			System.out.println(x);
+	int x = 2,y = 3;
+	x += y;
+	System.out.println(x);
 	MyFrame frame1 = new MyFrame();
 	frame1.setLocation(200,100);
 	frame1.setSize(150,150);
