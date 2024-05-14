ca3ccc8a4bd809a4e7bb68ee88c0304fb512f04d
MOTTTIRIomoti <zeal23310168@fsgcl.jp>
2024-05-14 11:54:47 +0900

    3.3追加

---
+++ 初級JAVA3.3/src/RectTest.java
@@ -0,0 +1,22 @@
+
+public class RectTest extends MyFrame{
+	public void run() {
+		
+		setColor(255,255,0);
+		fillRect(30, 40, 50, 100);
+		
+		setColor(255,0,255);
+		fillRect(130, 40, 50, 100);
+		
+		setColor(0,255,255);
+		fillRect(90, 160, 30, 30);
+		
+		setColor(0,0,0);
+		fillRect(30, 200, 150, 10);
+		
+		setColor(100,100,100);
+		fillRect(30, 250, 150, 10);
+		
+	}
+
+}
