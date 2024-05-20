89ff517a95845cb9ea31c4fbe2ecadf198ada73c
MOTTTIRIomoti <zeal23310168@fsgcl.jp>
2024-05-20 11:52:28 +0900

    確認問題4-2の追加

---
+++ 初級JAVA4-2/src/RectLoop.java
@@ -0,0 +1,13 @@
+
+public class RectLoop extends MyFrame{
+	public void run() {
+		int x = 30;
+		setColor(0,128,0);
+		for(int i = 0;i < 10; i++) {
+			System.out.println(x);
+			fillRect(x, 80, 10, 100);
+			x += 20;
+			}
+	}
+
+	}
