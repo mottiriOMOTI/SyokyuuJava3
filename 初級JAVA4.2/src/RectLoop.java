e89a6c993f53932beae84f5e84ddc8d3d1f88408
MOTTTIRIomoti <zeal23310168@fsgcl.jp>
2024-05-20 11:49:51 +0900

    no message

--- 初級JAVA4.2/src/RectLoop.java
+++ 初級JAVA4.2/src/RectLoop.java
@@ -3,7 +3,7 @@ public class RectLoop extends MyFrame{
 	public void run() {
 		int x = 30;
 		setColor(0,128,0);
-		while(x <= 210) {
+		while(x<=170) {
 		System.out.println(x);
 		fillRect(x, 80, 10, 100);
 		x += 20;
