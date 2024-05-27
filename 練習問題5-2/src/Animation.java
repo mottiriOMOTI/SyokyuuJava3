310388b87927cf895b444a04302b5a27f00176d4
MOTTTIRIomoti <zeal23310168@fsgcl.jp>
2024-05-27 11:13:47 +0900

    練習問題5-2修正

--- 練習問題5-2/src/Animation.java
+++ 練習問題5-2/src/Animation.java
@@ -26,6 +26,14 @@ public class Animation extends MyFrame{
 			sleep(0.1);
 			
 		}
+		while(x >= 30) {
+		clear();
+		setColor(0,128,0);
+		fillRect(x,y,10,100);
+		x -= 5;
+		sleep(0.1);
+		
+	}
 		
 	}
 
