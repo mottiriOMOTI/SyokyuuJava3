d5fc81bf12685288c178c3ef2ed1d1838ea19089
MOTTTIRIomoti <zeal23310168@fsgcl.jp>
2024-05-28 10:04:57 +0900

    ６．２追加

---
+++ 初級JAVA6.2/src/DisplayFaceObject.java
@@ -0,0 +1,11 @@
+
+public class DisplayFaceObject extends MyFrame{
+	public void run() {
+		Face face1 = new Face(30,50);
+		face1.draw(this);
+		
+		Face face2 = new Face(200,50);
+		face2.draw(this);
+	}
+
+}
