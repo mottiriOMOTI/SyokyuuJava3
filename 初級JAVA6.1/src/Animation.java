2a95955278caffa2b95b88223fc6f40818cd3263
MOTTTIRIomoti <zeal23310168@fsgcl.jp>
2024-05-28 09:57:27 +0900

    6.1追加

---
+++ 初級JAVA6.1/src/Animation.java
@@ -0,0 +1,43 @@
+
+public class Animation extends MyFrame{
+	public void run() {
+		int x = 30,y = 60;
+		while(true) {
+			
+		while(y <= 200) {
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			x += 5;
+			y += 5;
+			sleep(0.1);
+		}
+		while(y >= 60) {
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			y -= 5;
+			x += 5;
+			sleep(0.1);
+		}
+		while(y <= 200) {
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			x -= 5;
+			y += 5;
+			sleep(0.1);
+		}
+		while(y >= 60) {
+			clear();
+			setColor(0,128,0);
+			fillRect(x,y,10,100);
+			y -= 5;
+			x -= 5;
+			sleep(0.1);
+		}
+	}
+		
+	}
+
+}
