a740a83d45f588eac1754754a67b30fc854b3ac8
MOTTTIRIomoti <zeal23310168@fsgcl.jp>
2024-06-17 10:53:39 +0900

    初級9.1~9.2追加

---
+++ 初級JAVA9.1/src/Truck.java
@@ -0,0 +1,13 @@
+
+public class Truck extends Vehicle{
+	public Truck(int x,int y,int vx,int vy) {
+		super(x,y,vx,vy);
+	}
+	public void draw(MyFrame frame) {
+		frame.fillRect(x, y, 100, 30);
+		frame.fillRect(x+70, y-30, 30, 30);
+		frame.fillOval(x + 5, y+30, 10, 10);
+		frame.fillOval(x + 15, y+30, 10, 10);
+		frame.fillOval(x + 75, y+30, 10, 10);
+	}
+}
---
+++ 初級JAVA9.2/src/DisplayVehicleVector.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+public class DisplayVehicleVector extends MyFrame{
+	public void run() {
+		Vector<Vehicle> vehicles = new Vector<Vehicle>();
+		vehicles.add(new Car(10,30,3,0));
+		vehicles.add(new Car(100,100,-5,0));
+		vehicles.add(new Train(10,150,7,0));
+		vehicles.add(new Train(200,200,-7,0));
+		vehicles.add(new Truck(10,250,7,0));
+		vehicles.add(new Truck(100,300,7,0));
+		
+		for(int i=0;i<30;i++) {
+			clear();
+			for(int j=0;j<vehicles.size();j++) {
+				vehicles.get(j).draw(this);
+				vehicles.get(j).move();
+			}
+			sleep(0.1);
+		}
+	}
+
+}
